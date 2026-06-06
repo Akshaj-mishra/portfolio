b26140ca50022d9149796dd7b7238a6fee55271d
Akshaj Mishra <141712076+Akshaj-mishra@users.noreply.github.com>
2026-06-06 18:56:33 +0530

    Update personal.jsx

--- src/assets/personal.jsx
+++ src/assets/personal.jsx
@@ -100,7 +100,7 @@ export const personalData = {
       title: "MARGDARSHAN",
       description: "Markdarshan Truck Routing is an intelligent logistics solution that optimizes truck routes for faster, safer, and more cost-efficient transportation.",
       githubLink: "https://github.com/Akshaj-mishra/MARGDARSHAN",
-      liveLink: "https://markdershan-frontend.vercel.app/", 
+      liveLink: "https://markdershan-frontend.vercel.app/frontend/map_page_main", 
       imageUrl: markdarshan,
       techStack: ["Fast API", "Next", "Type Script", "MongoDB", "Xg Boast Model", "Google Maps API" , "Express"],
       points: [
